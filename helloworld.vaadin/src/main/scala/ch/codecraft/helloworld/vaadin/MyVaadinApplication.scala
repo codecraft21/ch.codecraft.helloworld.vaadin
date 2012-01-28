c1e2833c430cc1cb107f7d30693aac6e7bd057a3
Chris <chris@codecraft.ch>
2012-01-28 03:00:00 +0100

    OPEN - task 2: scala-wrappers button.addListener not working http://github.com/codecraft21/ch.codecraft.helloworld.vaadin/issues/issue/2

--- helloworld.vaadin/src/main/scala/ch/codecraft/helloworld/vaadin/MyVaadinApplication.scala
+++ helloworld.vaadin/src/main/scala/ch/codecraft/helloworld/vaadin/MyVaadinApplication.scala
@@ -1,23 +1,18 @@
 package ch.codecraft.helloworld.vaadin
 
-import com.vaadin.Application
-import com.vaadin.ui._
+import vaadin.scala._
 
-class MyVaadinApplication extends Application {
+class MyVaadinApplication extends com.vaadin.Application {
 
   def init(): Unit = {
-     setMainWindow(new Window("My Vaadin Application"))
-     val button = new Button("Click Me")
-     
-     //TODO find out how scala.swing handles SAM classes
-     button.addListener(new Button.ClickListener() {
-       def buttonClick(event: Button#ClickEvent): Unit = {
-         getMainWindow.addComponent(new Label("Thank you for clicking"))
-       }
-     })
-     //FIXME button.addListener(_ => getMainWindow.addComponent(new Label("Thank you for clicking")))
+    setMainWindow(new Window("My Vaadin Application"))
+    val button = new Button(caption = "Click Me", action = buttonAction)
 
-     getMainWindow.addComponent(button)
+    //button.addListener(_ => getMainWindow.addComponent(new Label("Thank you for clicking")))
+
+    getMainWindow.addComponent(button)
   }
+
+  def buttonAction(event: com.vaadin.ui.Button#ClickEvent) = getMainWindow.addComponent(new Label("Thank you for clicking"))
 
 }
