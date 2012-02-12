91c8c0f916457d2c576a5cc6370a609d32fd277c
Chris <chris@codecraft.ch>
2012-02-12 23:29:43 +0100

    following Book of Vaadin

--- helloworld.vaadin/src/main/java/ch/codecraft/helloworld/vaadin/MyVaadinApplication.java
+++ helloworld.vaadin/src/main/java/ch/codecraft/helloworld/vaadin/MyVaadinApplication.java
@@ -1,6 +1,10 @@
 package ch.codecraft.helloworld.vaadin;
 
+import java.util.Date;
+
 import com.vaadin.Application;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.Window;
 
@@ -10,9 +14,18 @@ public class MyVaadinApplication extends Application {
 
     @Override
     public void init() {
-        Window main = new Window("Hello window");
+        final Window main = new Window("Hello window");
         setMainWindow(main);
         main.addComponent(new Label("Hello World!"));
+
+        main.addComponent(new Button("what time is it?", new Button.ClickListener() {
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            public void buttonClick(ClickEvent event) {
+                main.showNotification("Time now " + new Date());
+            }
+        }));
     }
 
 }
